b5226f24481d2fff06fa285c01239b3184aab9c1
kubaslawski <kubaslawski@gmail.com>
2023-05-03 23:00:26 +0200

    A_Rule_of_Divisibility_by_13, Closures_and_Scopes, Pair_of_Gloves, Pyramind_Array, Validate_Credit_Card_Number

---
+++ 6kyu/Pair_of_Gloves.py
@@ -0,0 +1,26 @@
+"""
+DESCRIPTION:
+Pair of gloves
+Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+
+Examples:
+input = ["red", "green", "red", "blue", "blue"]
+result = 2 (1 red pair + 1 blue pair)
+
+input = ["red", "red", "red", "red", "red", "red"]
+result = 3 (3 red pairs)
+"""
+
+def number_of_pairs(gloves):
+    pairs = 0
+    dct = {}
+    for x in gloves:
+        if x in dct:
+            dct[x] += 1
+        else:
+            dct[x] = 1
+    for key, value in dct.items():
+        pairs += value // 2
+    return pairs
